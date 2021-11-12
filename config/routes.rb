b046503231783a152a02dd926262d996224bc1a0
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-11 19:49:30 -0500

    almost done user auth

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   get 'about' => 'about#show'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   namespace :admin do
     root to: 'dashboard#show'
