8f2d3ba148d34c731cfc8ac97ce784b53eaed57d
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-15 16:11:08 -0500

    new valid product to test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) {
+    Category.new(name: "testCat")
+  }
+  subject {
+    category.products.new(
+      name: "test",
+      description: "great for writing tests",
+      image: "this is a picture",
+      price: 9001,
+      quantity: 20,
+    )
+}
+
+  describe "Validations" do
+  
+  it 'should save product successfully if it has a name, price, quanity and category' do
+  expect(subject).to be_valid
+  expect(subject.errors.full_messages).to be_empty
+
+end
+end
 end
