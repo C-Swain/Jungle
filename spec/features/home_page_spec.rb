eef14f53c953774a874dd8bd77c9b7dbf1a86267
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-16 18:18:42 -0500

    Step 6 - More robust test -failing

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
+    101.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -24,6 +24,8 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     save_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product'
+
+    expect(page).to have_css 'article.product', count: 10
+  
   end
 end
