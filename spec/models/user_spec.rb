b78e42ae2d35ad34518801bd8829d4ab685d81a0
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-15 19:43:59 -0500

    writing the test questions first

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    # validation examples here
+ it "Should confirm the password and password confirmation field match"
   end
+
+ it "Should check to make sure the email is original/ doesn't already exist"
+
+end
+ 
+ 
+ it "The email is not case sensitive"
+end
+
+
 
   describe '.authenticate_with_credentials' do
     # examples for this class method here
