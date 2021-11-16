991b74f5424588849dbbf9f2612b4fc7136272e4
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-16 15:03:38 -0500

    user creation test done, time for authenticate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,23 +31,43 @@ end
     subject.valid?
     expect(subject.errors).not_to be_empty
   end
+
   it 'should not save when email field is empty' do
     subject.email = nil 
     subject.valid?
     expect(subject.errors).not_to be_empty
   end 
+
   it 'should not save when password field is empty' do
     subject.password = nil 
     subject.valid?
     expect(subject.errors).not_to be_empty
   end
+
   it 'should not save when password_confirmation field is empty' do
     subject.password_confirmation = nil 
     subject.valid?
     expect(subject.errors).not_to be_empty
   end
-  
-  
+
+  it 'should not create a user if the password and the password_confirmation do not match' do
+    subject.password_confirmation = 'creepypaper'
+    subject.valid?
+    expect(subject.errors).not_to be_empty
+  end
+
+  it 'shoule not allow a user to register with an email already in the system' do
+    User.create(                      
+              :first_name => 'Nandy',
+              :last_name => 'Relentless',
+              :email => 'Nandy@gmail.com',
+              :password => 'pillage',
+              :password_confirmation => 'pillage',
+              :password_digest => '222'
+    )   
+  subject.valid?
+  expect(subject.errors).not_to be_empty  
+
 end
 end
 end
